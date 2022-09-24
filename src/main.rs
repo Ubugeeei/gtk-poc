945622877037f924ef9fca5cb5d852b0d98a4b62
Ubugeeei <ubuge1122@gamil.com>
2022-09-24 13:44:52 +0900

    feat: running with args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
+use gtk::gio::ApplicationFlags;
+use gtk::prelude::*;
+use gtk::Application;
+
+mod button;
 mod window;
 
+const SAMPLE_PJ_NAME_HELP: &str = r#"
+SAMPLE_PJ_NAME:
+    - window
+    - button
+"#;
+
 fn main() {
-    window::run();
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() == 1 {
+        println!("Usage: cargo run [SAMPLE_PJ_NAME]");
+        println!("{}", SAMPLE_PJ_NAME_HELP);
+        return;
+    }
+
+    let build_ui = match &*args[1] {
+        "window" => window::build_ui,
+        "button" => button::build_ui,
+        _ => {
+            println!("Unknown sample_pj_name: {}", &args[1]);
+            println!("{}", SAMPLE_PJ_NAME_HELP);
+            panic!()
+        }
+    };
+
+    let app = Application::new(Some("com.example.App"), ApplicationFlags::HANDLES_OPEN);
+
+    // Connect to "activate" signal of `app`
+    // app.connect_activate(build_ui);
+    app.connect_open(build_ui);
+
+    // Run the application
+    // app.run_with_args(&args);
+    app.run();
 }
--- src/window.rs
+++ src/window.rs
@@ -1,18 +1,7 @@
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow};
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld2";
-
-pub fn run() {
-    // Create a new application
-    let app = Application::builder().application_id(APP_ID).build();
-    // Connect to "activate" signal of `app`
-    app.connect_activate(build_ui);
-    // Run the application
-    app.run();
-}
-
-fn build_ui(app: &Application) {
+pub fn build_ui(app: &Application, _files: &[gtk::gio::File], _blah: &str) {
     // Create a window and set the title
     let window = ApplicationWindow::builder()
         .application(app)
