3892e1796b989604fe5afef86bbf402b59a9df07
Ubugeeei <ubuge1122@gamil.com>
2022-09-24 13:44:41 +0900

    feat: button example

---
+++ src/button.rs
@@ -0,0 +1,29 @@
+use gtk::{prelude::*, Button};
+use gtk::{Application, ApplicationWindow};
+
+pub fn build_ui(app: &Application, _files: &[gtk::gio::File], _blah: &str) {
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(move |button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Present window
+    window.present();
+}
